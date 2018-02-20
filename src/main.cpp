08c251addc4999799a8f957fcad6ccb0d18fe3c9
ilkunviktor <35297455+ilkunviktor@users.noreply.github.com>
2018-02-20 17:07:20 +0200

    + init

---
+++ src/main.cpp
@@ -0,0 +1,98 @@
+﻿#include <chrono>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <deque>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <assert.h>
+#include <string>
+#include <ctime>
+#include <iomanip>
+#include <algorithm>
+#include <memory>
+#include <numeric>
+#include <filesystem>
+
+using namespace std;
+
+using uint = uint64_t;
+
+struct BestObject
+{
+	uint id = 0;
+	uint weight = 0;
+	uint score = 0;
+};
+
+vector<BestObject> FindBestObjects(uint weightFree, vector<BestObject>& objects)
+{
+	vector<BestObject> result;
+
+	return result;
+}
+
+int main()
+{
+	string inPath = "../../in";
+	string outPath = "../../out";
+	uint64_t scoreTotal = 0;
+	uint64_t millisecondsTotal = 0;
+
+	for (const auto& taskPath : experimental::filesystem::v1::directory_iterator(inPath))
+	{
+		chrono::time_point<std::chrono::system_clock> timeStart =
+			chrono::system_clock::now();
+
+		stringstream ss;
+		ss << taskPath;
+		string filePathIn = ss.str();
+
+		size_t fileNameStart = filePathIn.find_last_of('\\');
+		size_t fileNameEnd = filePathIn.find_last_of('.');
+		string fileName = filePathIn.substr(fileNameStart + 1, fileNameEnd - fileNameStart - 1);
+
+		string filePathOut = outPath + "/" + fileName + ".out";
+
+		// input
+		ifstream fileIn;
+		fileIn.open(filePathIn);
+		assert(fileIn.is_open());
+
+		//fileIn >>
+
+		fileIn.close();
+
+		// solve
+
+
+		// output
+		ofstream fileOut;
+		fileOut.open(filePathOut);
+		assert(fileOut.is_open());
+
+		// fileOut <<  << endl;
+
+		fileOut.close();
+
+		// score
+		uint64_t score = 0;
+		scoreTotal += score;
+		// timing
+		chrono::time_point<std::chrono::system_clock> timeEnd =
+			chrono::system_clock::now();
+		chrono::milliseconds millisec = chrono::duration_cast<chrono::milliseconds>(timeEnd - timeStart);
+		millisecondsTotal += millisec.count();
+		cout << "file: " << fileName << endl;
+		cout << "score: " << score << endl;
+		cout << "time(ms): " << to_string(millisec.count()) << endl << endl;
+	}
+
+	cout << "scoreTotal: " << scoreTotal << endl;
+	cout << "millisecondsTotal: " << millisecondsTotal << endl << endl;
+	system("pause");
+
+	return 0;
+}
